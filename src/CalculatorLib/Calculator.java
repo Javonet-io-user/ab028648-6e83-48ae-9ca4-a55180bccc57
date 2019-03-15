5df1656f90c87d8f9f112377b64f0dd262f5f3b5
Javonet <support@javonet.com>
2019-03-15 21:39:41 +0000

    init

---
+++ src/CalculatorLib/Calculator.java
@@ -0,0 +1,62 @@
+package CalculatorLib;
+
+import Common.Activation;
+import static Common.JavonetHelper.Convert;
+import static Common.JavonetHelper.getGetObjectName;
+import static Common.JavonetHelper.getReturnObjectName;
+import static Common.JavonetHelper.ConvertToConcreteInterfaceImplementation;
+import Common.JavonetHelper;
+import Common.MethodTypeAnnotation;
+import com.javonet.Javonet;
+import com.javonet.JavonetException;
+import com.javonet.JavonetFramework;
+import com.javonet.api.NObject;
+import com.javonet.api.NEnum;
+import com.javonet.api.keywords.NRef;
+import com.javonet.api.keywords.NOut;
+import com.javonet.api.NControlContainer;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.Iterator;
+import java.lang.*;
+import CalculatorLib.*;
+
+public class Calculator {
+  protected NObject javonetHandle;
+
+  public Calculator() {
+    try {
+      javonetHandle = Javonet.New("CalculatorLib.Calculator");
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+    }
+  }
+
+  public Calculator(NObject handle) {
+    this.javonetHandle = handle;
+  }
+
+  public void setJavonetHandle(NObject handle) {
+    this.javonetHandle = handle;
+  }
+  /** Method */
+
+  @MethodTypeAnnotation(type = "Method")
+  public java.lang.Integer Sum(java.lang.Integer a, java.lang.Integer b) {
+    try {
+      java.lang.Integer res = javonetHandle.invoke("Sum", a, b);
+      if (res == null) return 0;
+      return res;
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+      return 0;
+    }
+  }
+
+  static {
+    try {
+      Activation.initializeJavonet();
+    } catch (java.lang.Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
